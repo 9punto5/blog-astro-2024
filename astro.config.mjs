6791b9716232cb28067bd2e29547889166377b82
fabianhtml <fabianhtml@gmail.com>
2025-02-20 17:58:10 -0300

    fix: configurar directorio de salida para Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,23 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import AutoImport from "astro-auto-import";
-import { defineConfig, squooshImageService } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
 import config from "./src/config/config.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
-  base: config.site.base_path ? config.site.base_path : "/",
+  site: config.site.base_url,
+  base: config.site.base_path,
   trailingSlash: config.site.trailing_slash,
+  outDir: './build/blog',
   image: {
-    service: squooshImageService(),
+    service: { entrypoint: "astro/assets/services/sharp" },
+  },
+  build: {
+    format: 'directory'
   },
   integrations: [
     react(),
